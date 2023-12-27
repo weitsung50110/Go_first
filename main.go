12dcc31fed3bfcaa1c0ec9319dd630b4504075cb
weitsung50110 <a1233z51ab10@gmail.com>
2023-12-27 10:51:57 +0800

    add input name and server change to json

--- main.go
+++ main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin" //導入GIN
+	"fmt"
+
+	"github.com/gin-gonic/gin" // 導入 Gin 框架
 )
 
 func main() {
+	// 建立一個 Gin 的預設路由器
 	router := gin.Default()
 
-	// 伺服器回傳 index.html
+	// 設定靜態檔案伺服器，指定靜態檔案的存放路徑為 "./static"
+	router.Static("./static", "./static")
+
+	// 設定一個 GET 請求路由，當訪問根目錄時，回傳 index.html 檔案
 	router.GET("/", func(c *gin.Context) {
 		c.File("templates/index.html")
 	})
 
-	// 從 POST 請求中獲取 inputText
+	// 設定一個 POST 請求路由，處理來自 /submit 路徑的 POST 請求
 	router.POST("/submit", func(c *gin.Context) {
+		// 從 POST 請求中獲取名為 inputText 和 inputName 的表單數據
 		inputText := c.PostForm("inputText")
-		// 將 inputText 回傳給前端
-		c.String(200, inputText)
-	})
+		inputName := c.PostForm("inputName")
+		fmt.Println("inputName : ", inputName) // 將 inputName 印出到終端機上
 
-	// 設定靜態檔案伺服器
-	router.Static("/static", "./static")
+		// 將資料封裝為 JSON 格式，使用 gin 框架的 gin.H(map) 型別
+		responseData := gin.H{
+			"inputText": inputText,
+			"inputName": inputName,
+		}
+
+		// 將 JSON 格式的資料作為回應返回給前端
+		c.JSON(200, responseData)
+	})
 
 	// 啟動伺服器，監聽端口 8080
 	router.Run(":8080")
--- static/script.js
+++ static/script.js
@@ -1,20 +1,27 @@
 // JavaScript 檔案
 document.getElementById("textForm").addEventListener("submit", function(event) {
     event.preventDefault(); // 阻止表單預設提交行為
-    let inputText = document.getElementById("inputText").value; // 獲取表單輸入框中的文字
+
+    const formData = {
+        // 獲取表單中的數據並添加到 FormData 對象中
+        inputText: document.getElementById("inputText").value, // 獲取表單輸入框中的文字,
+        inputName: document.getElementById('inputName').value
+    }
+    console.log("123",formData)
 
     const requestOptions = {
         method: "POST", // 使用 POST 方法
-        body: new URLSearchParams({ inputText: inputText }), // 提交表單文字
+        body: new URLSearchParams(formData), // 提交表單文字
         headers: {
             "Content-Type": "application/x-www-form-urlencoded" // 設置標頭內容類型
         }
     }
 
     fetch("/submit",requestOptions)
-    .then(response => response.text()) // 獲取伺服器返回的文字內容
+    .then(response => response.json()) // 獲取伺服器返回的文字內容
     .then(data => {
-        document.getElementById("textDisplay").innerHTML = data; // 將文字內容顯示在 id 為 textDisplay 的區域
+        document.getElementById("textDisplay").innerHTML =data.inputName+" : "+data.inputText; 
+        // 將文字內容顯示在 id 為 textDisplay 的區域
     });
 });
 
